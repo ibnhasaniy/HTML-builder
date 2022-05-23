6c4aa92a3d211f5c18ae65c94bf08e948ed6ccfc
Muhammadyusuf <ibnhasaniy@gmail.com>
2022-05-23 17:51:57 +0500

    feat: solve 04-copy-directory

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,19 +6,19 @@ const FOLDER = path.join(__dirname, '/secret-folder');
 fs.readdir(FOLDER, { withFileTypes: true }, (err, items) => {
   if (err) throw err;
 
-  for(let i=0;i<items.length;i++){
+  for (let i = 0; i < items.length; i++) {
     //   get all item names inside for loop
-    let itemName=path.join(FOLDER,items[i].name);
-    fs.stat(itemName,(err,stats)=>{
-      if(err)throw err;
-      if(stats.isFile()){
+    let itemName = path.join(FOLDER, items[i].name);
+    fs.stat(itemName, (err, stats) => {
+      if (err) throw err;
+      if (stats.isFile()) {
         //   parsedData returns object
-        let parsedData= path.parse(items[i].name);
+        let parsedData = path.parse(items[i].name);
         let fileName = parsedData.name;
         // split by dot and take second  index which is extension of file
         let fileExtension = parsedData.ext.split('.')[1];
         // stats.size will return bytes. Convert to KB and fixed it with .toFixed() method
-        let sizeOfFile = (stats.size / 1024).toFixed(1);
+        let sizeOfFile = (stats.size / 1024).toFixed(3);
 
         console.log(`${fileName} - ${fileExtension} - ${sizeOfFile}KB`);
       }
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require('fs/promises');
+const path = require('path');
 
+const SRC_PATH = path.join(__dirname, '/files');
+const DEST_PATH = path.join(__dirname, '/files-copy');
+
+copyDir();
+
+async function copyDir() {
+  fs.mkdir(DEST_PATH, { recursive: true });
+  let files = await fs.readdir(SRC_PATH, { withFileTypes: true });
+
+  for (let i = 0; i < files.length; i++) {
+    if (files[i].isFile) {
+      let srcFile = path.join(SRC_PATH, files[i].name);
+      let destFile = path.join(DEST_PATH, files[i].name);
+      fs.copyFile(srcFile, destFile);
+    }
+  }
+}
